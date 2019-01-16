39085cd4d30a564afae94996388b8ce5dc8abc0a
IKunDefined <1245729251@qq.com>
2019-01-16 13:41:15 +0800

    repo init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="wrap">
+    <h1>My Todo List</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#wrap {
+  width: 1000px;
+  margin: 0 auto;
+}
+
+h1 {
+  color: #888;
+  font-size: 20px;
+  text-align: center;
+}
+</style>
